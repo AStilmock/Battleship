8d359b04ee46ef8a7a700b3627cde5ff01d5c205
Jesse Sorman <jessesorman@gmail.com>
2023-04-06 17:32:33 -0400

    feat ship health and hits

--- lib/ship.rb
+++ lib/ship.rb
@@ -3,5 +3,22 @@ class Ship
   def initialize(name, length)
     @name = name
     @length = length
+    @health = length
+  end
+
+  def health
+    @health
+  end
+
+  def sunk?
+    if health > 0
+      false
+    else
+      true
+    end
+  end
+
+  def hit
+    @health -= 1
   end
 end
