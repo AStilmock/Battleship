68e0a353671641e785743b34b5ee62f57c854a2b
Jesse Sorman <jessesorman@gmail.com>
2023-04-06 17:02:15 -0400

    feat ship class initialize

---
+++ spec/ship_spec.rb
@@ -0,0 +1,9 @@
+require "./lib/ship"
+
+RSpec.describe Ship do
+  it "can initialize" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.name).to eq("Cruiser")
+    expect(cruiser.length).to eq(3)
+  end
+end
