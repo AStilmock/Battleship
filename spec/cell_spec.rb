d002e4f3a682ede1052b9bfbaed8ce145689beae
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-04-06 16:04:52 -0600

    Add code to complete cell object testing

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -10,13 +10,24 @@ RSpec.describe Cell do
   describe "initialize" do
     it "exists" do
       expect(@cell).to be_an_instance_of(Cell)
-      expect(@cruiser).to be_an_instance_of(Ship)
     end
 
     it "checks cell data" do
       expect(@cell.coordinate).to eq("B4")
       expect(@cell.ship).to eq(nil)
       expect(@cell.empty?).to eq(true)
+    end
+  end
+
+  describe "checks ship data" do
+    it "exists" do
+      expect(@cruiser).to be_an_instance_of(Ship)
+    end
+    it "places a ship" do
+      @cell.place_ship(@cruiser)
+      expect(@cell.ship).to eq(@cruiser)
+      expect(@cell.empty?).to eq(false)
+      require 'pry'; binding.pry
     end
   end
 
